ea62c62801a88cf7cef151983b08a5a22ab4f7d3
Emma <emma.deoliveira@ynov.com>
2026-05-06 09:38:17 +0200

    Fix(index.js): add user, profile, favorite, follower

--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,23 @@ const sequelize = require("../config/db");
 
 const models = {};
 
-// Imports de Models
 models.User = require("./UserModel")(sequelize);
-models.Ingredient = require("./IngredientModel");
+models.Ingredient = require("./IngredientModel")(sequelize);
+models.Profile = require("./ProfileModel")(sequelize);
+models.Favorite = require("./FavoriteModel")(sequelize);
+models.Follower = require("./FollowerModel")(sequelize);
 
+// User <-> Profile
+models.User.hasOne(models.Profile, { foreignKey: "user_id" });
+models.Profile.belongsTo(models.User, { foreignKey: "user_id" });
+
+// Profile <-> Favorite
+models.Profile.hasMany(models.Favorite, { foreignKey: "profile_id" });
+models.Favorite.belongsTo(models.Profile, { foreignKey: "profile_id" });
+
+// Profile <-> Follower
+models.Profile.hasMany(models.Follower, { foreignKey: "following_id", as: "followers" });
+models.Profile.hasMany(models.Follower, { foreignKey: "follower_id", as: "following" });
 
 Object.keys(models).forEach((modelName) => {
     if (models[modelName].associate) {
